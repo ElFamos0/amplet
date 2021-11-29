b23af14278d7e1aec8d3de6b71153b88b4b1073e
Yewolf <yyewolf@gmail.com>
2021-11-30 00:48:48 +0100

    [MODIF] Structure

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import *
 import os
 
 # Setup ####################################
@@ -10,16 +9,15 @@ if not os.path.isfile("data/db.db"):
     setup = True
 ############################################
 
-app = Flask(__name__) # We define the flask application on main
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/db.db' # Declares the database path
-db = SQLAlchemy(app) # Uses SQLAlchemy as ORM
+from db import *
+from models import *
         
 db.create_all() # Creates the tables if necessary
 
 # Setup ####################################
 if setup:
-    admin = User(username='admin', email='admin@test.com')
-    guest = User(username='guest', email='guest@test.com')
+    admin = users.User(username='admin', email='admin@test.com')
+    guest = users.User(username='guest', email='guest@test.com')
     db.session.add(admin)
     db.session.add(guest)
     db.session.commit()
@@ -29,7 +27,7 @@ if setup:
 def hello_world():
     content = "<p>Hello, World ! Here are my users : </p>"
     content += "<br/>"
-    for user in User.query.all():
+    for user in users.User.query.all():
         content += f"{user.id} - {user.username} & {user.email}"
         content += "<br/>"
     return content
