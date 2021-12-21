40742fa4cef4032becfcba6b2af408be977328b9
Capelli <louis-vincent.capelli@telecomnancy.eu>
2021-12-21 17:44:42 +0100

    fonction score_course et lien entre login et register

---
+++ app/score_course.py
@@ -0,0 +1,10 @@
+
+# calcule le score rapporté par une course en fonction de
+# nb_mag : le nombre de magasins visités par le coursier
+# nb_clients : le nombre de clients qui ont bénéficié de la course
+def score_course(nb_mag, nb_clients):
+    if nb_mag<0:
+        raise ValueError("nombre de magasins visités négatif")
+    if nb_clients<0:
+        raise ValueError("nombre de clients négatif")
+    return nb_clients*nb_mag
