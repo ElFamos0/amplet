4c66329ed5b9cdb20f3a6c3d5f6b94f2bf7a66b2
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2021-12-23 21:59:44 +0100

    [MODIF] Restructuration des fichiers

---
+++ app/routes/avatar.py
@@ -0,0 +1,35 @@
+from db import app
+from flask_login import login_required, current_user
+from flask import render_template, request, send_from_directory
+import os
+
+default_path = "photos/chien.jpeg"
+
+
+##############################
+########### AVATAR ###########
+##############################
+
+@app.route('/change_avatar', methods = ['GET', 'POST'])
+@login_required
+def upload_pp():
+    if request.method == 'POST':
+        f = request.files['file']
+        folder = f"static/avatar/{current_user.id}"
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        f.save(f"{folder}/avatar.png")
+        return 'avatar reçu !'
+    else:
+        return render_template('upload.html', personne=current_user)
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+@app.route('/r/a/<string:id>')
+def get_pp(id):
+    folder = f"static/avatar/{id}"
+    file = f"avatar/{id}/avatar.png"
+    if not os.path.exists(folder):
+        return send_from_directory('static', default_path)
+    return send_from_directory('static', file)
