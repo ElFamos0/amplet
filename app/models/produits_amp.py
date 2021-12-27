f240f93ef3935b8ec4a65e1ea2be317818b2eb52
Malo DAMIEN <malo.damien@telecomnancy.eu>
2021-12-27 23:23:18 +0000

    [AJOUT] "Vrai" page admin et changement db

--- app/models/produits_amp.py
+++ app/models/produits_amp.py
@@ -3,7 +3,7 @@ from app import db
 class Produits_amp(db.Model):
     __tablename__ = 'produits_amp'
     id_amp = db.Column(db.String(40), primary_key=True)
-    id_produit = db.Column(db.String(40), db.ForeignKey('produits.id'))
+    id_produit = db.Column(db.String(40), db.ForeignKey('produits.id'),primary_key=True)
     quantite = db.Column(db.Integer)
     unite = db.Column(db.String(10))
     user_id = db.Column(db.String(40), db.ForeignKey('users.id'))
