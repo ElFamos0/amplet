c3311b0436ff7bdd6c38bbd03b6fe55d1a4b7305
Malo DAMIEN <malo.damien@telecomnancy.eu>
2021-12-28 15:45:12 +0000

    [AJOUT] Page commande

---
+++ app/routes/commande.py
@@ -0,0 +1,14 @@
+from flask_login.utils import login_required
+from db import *
+from models import *
+from flask_login import current_user
+from flask import render_template
+
+##############################
+########### COMMANDE  ###########
+##############################
+
+@app.route("/commande")
+@login_required
+def commande():
+    return "Fait chauffer le camion marcel"
