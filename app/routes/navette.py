8308ca6c2240a9c4bcc252e3d761456c4cb2d883
Malo DAMIEN <elfamoso07@protonmail.com>
2021-12-28 23:13:41 +0000

    [MODIF] form navette finalisé

--- app/routes/navette.py
+++ app/routes/navette.py
@@ -12,11 +12,11 @@ from utils import timestamp
 def navette():
     L = {'username':str(current_user.username),'mail':str(current_user.email),'id':str(current_user.id)}
     listeproduits = []
-    listenavettes = []
+    listenavettes = {}
     for produit in produits.Produits.query.all():
         listeproduits.append(produit.nom)
     for navette in amplet.Amplets.query.filter(amplet.Amplets.navette==True).all():
-        listenavettes.append(timestamp.timestamp_to_date(navette.date_arrivee, format=True))
+        listenavettes[timestamp.timestamp_to_date(navette.date_arrivee, format=True)]= navette.id
     if request.method=='POST':
         items = []
         for i in range(0,5):
@@ -29,8 +29,8 @@ def navette():
             if item["produit"] not in listeproduits or 0 >= int(item["quantite"]) > 40:
                 continue
             idproduit = produits.Produits.query.filter(produits.Produits.nom==item["produit"]).first()
-            produit = produits_amp.Produits_amp(id_amp=57,id_produit=idproduit.id,quantite=int(item["quantite"]),unite=item["unite"],id_user=0)
+            produit = produits_amp.Produits_amp(id_amp=listenavettes[request.form.get("navette")],id_produit=idproduit.id,quantite=int(item["quantite"]),unite=item["unite"],id_user=current_user.id)
             db.session.add(produit)
             db.session.commit()
         return "Commande effectué"
-    return render_template("navette.html",personne=L,produits=listeproduits,optionnavette=listenavettes)
+    return render_template("navette.html",personne=L,produits=listeproduits,optionnavette=listenavettes.keys())
