d28d2efcebc5b95dee10b663d00cf326e06a2cab
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2021-12-30 16:58:38 +0100

    [MODIF] Clé primaire des marchands enregistrés dans des amplets

--- app/models/marchands_amp.py
+++ app/models/marchands_amp.py
@@ -2,5 +2,5 @@ from app import db
 
 class Marchands_amp(db.Model):
     __tablename__ = 'marchands_amp'
-    id_marchand = db.Column(db.String(40), primary_key=True)
-    id_amp = db.Column(db.String(40), db.ForeignKey('amplets.id'))
+    id_marchand = db.Column(db.String(40), db.ForeignKey('marchands.id'), primary_key=True)
+    id_amp = db.Column(db.String(40), db.ForeignKey('amplets.id'), primary_key=True)
