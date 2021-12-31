fecb9900a713d498dbc52728daa273a23539c383
Malo DAMIEN <elfamoso07@protonmail.com>
2021-12-31 11:40:10 +0000

    [FIX] Correction DB

--- app/models/produits_amp.py
+++ app/models/produits_amp.py
@@ -4,6 +4,6 @@ class Produits_amp(db.Model):
     __tablename__ = 'produits_amp'
     id_amp = db.Column(db.String(40), primary_key=True)
     id_produit = db.Column(db.String(40), db.ForeignKey('produits.id'),primary_key=True)
-    id_user = db.Column(db.String(40), db.ForeignKey('users.id'))
+    id_user = db.Column(db.String(40), db.ForeignKey('users.id'), primary_key=True)
     quantite = db.Column(db.Integer)
     unite = db.Column(db.String(10))
