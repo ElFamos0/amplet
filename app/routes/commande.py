ad5700c8b850bfa539727c9b6945fce9717b6745
Malo DAMIEN <elfamoso07@protonmail.com>
2022-01-02 14:10:58 +0000

    [AJOUT] début de la page mesamplet pour LV

--- app/routes/commande.py
+++ app/routes/commande.py
@@ -11,9 +11,14 @@ from flask import render_template
 @app.route("/commande")
 @login_required
 def commande():
+    ampletinscrit = []
+    ampletcoursier = []
+    inscriptions = participants_amp.Participants_amp.query.filter_by(id_user=current_user.id).all()
+    for i in range(len(inscriptions)):
+        ampletinscrit.append(inscriptions[i].id_amp)
+
     #faut afficher les amplet sur lesquelles l'utilisateur a été accepté
     # + une fois que Jules aura fait le classement, les produits des commandes navettes qui sont dispo dans les masagasins selectionnés
 
-    #je le fais en  rentrant de la salle, faut que je trouve comment faire des join mdr
     
-    return render_template("commande.html",moi=current_user)
+    return render_template("commande.html",moi=current_user,inscription=ampletinscrit,coursier=ampletcoursier)
