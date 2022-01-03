688a7071f6afa26a2bbab9e00d4bd30e8ece35fc
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-01-03 18:39:53 +0100

    [AJOUT] Les Amplets proposés sont maintenant visibles dans Mes Amplets

--- app/routes/commande.py
+++ app/routes/commande.py
@@ -1,4 +1,5 @@
 from flask_login.utils import login_required
+from models.users import User
 from db import *
 from models import *
 from flask_login import current_user
@@ -15,7 +16,6 @@ def commande():
     inscr_id_amp = []
     inscr_valide = []
     inscr_nom_coursier = []
-    cours_id_amp = []
     inscriptions = participants_amp.Participants_amp.query.filter_by(id_user=current_user.id).all()
     for i in range(len(inscriptions)):
         temp = inscriptions[i]
@@ -29,6 +29,17 @@ def commande():
         inscr_id_coursier.append(amplet.Amplets.query.filter_by(id = inscr_id_amp[-1], navette = 0).first().id_coursier)
         inscr_nom_coursier.append(users.User.query.filter_by(id = inscr_id_coursier[-1]).first().username)
     n = len(inscriptions)
+    cours_id_amp = []
+    cours_id_participants = []
+    cours_status_participants = []
+    cours_nom_participants = []
+    coursier = amplet.Amplets.query.filter_by(id_coursier = current_user.id).all()
+    for i in range(len(coursier)):
+        cours_id_amp.append(coursier[i].id)
+        cours_id_participants.append([e.id_user for e in participants_amp.Participants_amp.query.filter_by(id_amp = cours_id_amp[-1])])
+        cours_status_participants.append([e.valide for e in participants_amp.Participants_amp.query.filter_by(id_amp = cours_id_amp[-1])])
+        cours_nom_participants.append([users.User.query.filter_by(id = e).first().username for e in cours_id_participants[-1]])
+    m = len(coursier)
 
 
         
@@ -40,4 +51,5 @@ def commande():
     # + une fois que Jules aura fait le classement, les produits des commandes navettes qui sont dispo dans les masagasins selectionnés
 
     
-    return render_template("commande.html",moi=current_user,n=n,inscr_id_amp=inscr_id_amp, inscr_valide=inscr_valide,inscr_id_coursier=inscr_id_coursier,inscr_nom_coursier=inscr_nom_coursier)
+    return render_template("commande.html",user=current_user,n=n,inscr_id_amp=inscr_id_amp, inscr_valide=inscr_valide,inscr_id_coursier=inscr_id_coursier,inscr_nom_coursier=inscr_nom_coursier, 
+    cours_id_amp=cours_id_amp, cours_id_participants=cours_id_participants, cours_nom_participants=cours_nom_participants,cours_status_participants=cours_status_participants)
