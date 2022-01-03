efb1494e08d0b65afff93e3ce19722e741e8c7ae
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-01-03 17:19:41 +0100

    Update commande : Amplet inscrits

--- app/routes/commande.py
+++ app/routes/commande.py
@@ -11,14 +11,31 @@ from flask import render_template
 @app.route("/commande")
 @login_required
 def commande():
-    ampletinscrit = []
-    ampletcoursier = []
+    inscr_id_coursier = []
+    inscr_id_amp = []
+    inscr_valide = []
+    inscr_nom_coursier = []
+    cours_id_amp = []
     inscriptions = participants_amp.Participants_amp.query.filter_by(id_user=current_user.id).all()
     for i in range(len(inscriptions)):
-        ampletinscrit.append(inscriptions[i].id_amp)
+        temp = inscriptions[i]
+        inscr_id_amp.append(temp.id_amp)
+        if temp.valide==0:
+            inscr_valide.append("En attente")
+        if temp.valide==1:
+            inscr_valide.append("Accepté")
+        if temp.valide==2:
+            inscr_valide.append("Refusé")
+        inscr_id_coursier.append(amplet.Amplets.query.filter_by(id = inscr_id_amp[-1]).first().id_coursier)
+        inscr_nom_coursier.append(users.User.query.filter_by(id = inscr_id_coursier[-1]).first().username)
+    n = len(inscriptions)
+        
+    
+
+
 
     #faut afficher les amplet sur lesquelles l'utilisateur a été accepté
     # + une fois que Jules aura fait le classement, les produits des commandes navettes qui sont dispo dans les masagasins selectionnés
 
     
-    return render_template("commande.html",moi=current_user,inscription=ampletinscrit,coursier=ampletcoursier)
+    return render_template("commande.html",moi=current_user,n=n,inscr_id_amp=inscr_id_amp, inscr_valide=inscr_valide,inscr_id_coursier=inscr_id_coursier,inscr_nom_coursier=inscr_nom_coursier)
