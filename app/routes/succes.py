bae7ff9aad40f7dab882fd44b23a13480699554f
Jules BRUNET <jules.brunet@telecomnancy.eu>
2022-01-04 11:48:17 +0100

    [AJOUT] Error 404 handler

--- app/routes/succes.py
+++ app/routes/succes.py
@@ -2,7 +2,7 @@ from db import *
 from models import *
 from flask_login import login_required
 from flask_login import current_user
-from flask import render_template, request
+from flask import render_template
 from datetime import date, datetime
 from time import mktime
 from utils.timestamp import now, timestamp_to_date
@@ -13,5 +13,11 @@ from utils.amplets_a_afficher import amplets_a_afficher, amplet_dict
 @login_required
 def succes() :
 
-    return render_template('succes.html',user=current_user)
+    return render_template('succes.html',user=current_user,succesnavette = False)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    
+    return render_template('erreur.html',user=current_user,erreur= "Oops... Cette page n'existe pas !"), 404
 
