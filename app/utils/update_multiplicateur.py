e959db18d297731bfc57cc4b13d5d7990f03f76c
Jules BRUNET <jules.brunet@telecomnancy.eu>
2022-01-04 20:45:59 +0100

    [AJOUT] Fonction multiplicateur

---
+++ app/utils/update_multiplicateur.py
@@ -0,0 +1,22 @@
+from db import *
+from models import *
+
+def update_multiplicateur(liste_id_marchand,dic_marchand) :
+
+    l_marchand = marchands.Marchands.query.all()
+    l_non_select = []
+    vote_tot = 0
+    
+    for march in l_marchand :
+        if march.id in liste_id_marchand :
+            march.multiplicateur = 1
+        else :
+            l_non_select.append(march)
+            vote_tot += dic_marchand[march.id]
+        
+    for march in l_non_select :
+
+        march.multiplicateur *= (1 + dic_marchand[march.id]/vote_tot)
+    
+    db.session.commit()
+
