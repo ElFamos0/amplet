79d6222eb862e7c84afc6278824f94505d6b7efc
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-01-04 02:53:43 +0100

    [MODIF] recréation du .py pour nouvelleAmplet

--- app/routes/amplets.py
+++ app/routes/amplets.py
@@ -13,18 +13,6 @@ from utils.amplets_a_afficher import amplets_a_afficher, amplet_dict
 ########### AMPLETS  ###########
 ##############################
 
-@app.route('/nouvelleAmplet', methods=['GET','POST'])
-@login_required
-def nouvelleAmplet():
-    mag_dispo=['primeur du coin', 'chez Tony']
-    mag_visit=[]
-    # if request.method=='POST':
-    #     L=request.form
-    #     for e in mag_dispo:
-    #         if e in L:
-    #             mag_visit.append(e)
-    #     return mag_visit[1]
-    return render_template('nouvelleAmplet.html', magasins=mag_dispo)
 
 @app.route('/amplets_en_cours', methods=['GET','POST'])
 @login_required
---
+++ app/routes/nouvelleAmplet.py
@@ -0,0 +1,23 @@
+from db import *
+from models import *
+from flask_login import login_required
+from flask_login import current_user
+from flask import render_template, request
+from datetime import date, datetime
+from time import mktime
+from utils.timestamp import now, timestamp_to_date
+from utils.recherche_par import recherche_par
+from utils.amplets_a_afficher import amplets_a_afficher, amplet_dict
+
+@app.route('/nouvelleAmplet', methods=['GET','POST'])
+@login_required
+def nouvelleAmplet():
+    mag_dispo=['primeur du coin', 'chez Tony']
+    mag_visit=[]
+    # if request.method=='POST':
+    #     L=request.form
+    #     for e in mag_dispo:
+    #         if e in L:
+    #             mag_visit.append(e)
+    #     return mag_visit[1]
+    return render_template('nouvelleAmplet.html', magasins=mag_dispo)
