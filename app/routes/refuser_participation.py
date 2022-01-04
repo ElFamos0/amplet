71ba609ffde224fd98e67d01df015f5eda01645c
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-01-04 17:31:07 +0100

    [AJOUT] Possibilité de refuser dans participants dans Mes Amplets

---
+++ app/routes/refuser_participation.py
@@ -0,0 +1,26 @@
+from flask_login.utils import login_required
+from db import *
+from models import *
+from flask_login import current_user
+from flask import render_template
+
+from flask_login.utils import login_required
+from db import *
+from models import *
+from flask_login import current_user
+from flask import render_template
+from routes.commande import commande
+
+@app.route('/r/<string:id_ampl>/<string:id_part>')
+@login_required
+def refuser_participation(id_ampl,id_part):
+    curr_amp = participants_amp.Participants_amp.query.filter_by(id_user = id_part, id_amp=id_ampl).first()
+    curr_amp_id = curr_amp.id_amp
+    amp = amplet.Amplets.query.filter_by(id = curr_amp_id).first()
+    cours_id = amp.id_coursier
+    if curr_amp and current_user.id==cours_id:
+        curr_amp.valide=2
+        db.session.commit()
+        return commande()
+    else:
+        return render_template('erreur.html', erreur="Vous n'avez pas l'autorisation de modifier le status de cette Amplet ou elle n'existe pas",user=current_user)
