dc3fd82d88241b92043accf59490c6c1f64a59cc
Malo DAMIEN <elfamoso07@protonmail.com>
2022-01-04 16:48:04 +0000

    [MODIF] Lisibilité dashboard

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -12,27 +12,27 @@ def hello_world():
     content = "<p>UTILISATEURS</p>"
     content += "<br/>"
     for user in users.User.query.all():
-        content += f"{user.id} - {user.username} & {user.email}"
+        content += f"ID USER : {user.id} - USERNAME : {user.username} - EMAIL: {user.email}"
         content += "<br/>"
     content += "<br>"
     content += "<p>AMPLET</p>"
     content += "<br/>"
     for amplit in amplet.Amplets.query.all():
-        content += f"{amplit.id} - {amplit.date_depart} - {amplit.date_arrivee} - {amplit.places_dispo} - {amplit.id_coursier}"
+        content += f"ID AMPLET : {amplit.id} - DATE DEPART{amplit.date_depart} - DATE ARRIVE{amplit.date_arrivee} - PLACES DISPO (MAX) : {amplit.places_dispo} - ID COURSIER : {amplit.id_coursier}"
         content += "<br/>"
     content += "<br>"
     content += "<p>COMMANDES</p>"
     content += "<br/>"
     for produit_amp in produits_amp.Produits_amp.query.all():
         requete1 = users.User.query.filter(users.User.id==produit_amp.id_user).first()
-        content += f"{produit_amp.id_amp} - {produit_amp.id_produit} - {produit_amp.quantite} - {produit_amp.unite} - {requete1.username}"
+        content += f"ID AMPLET : {produit_amp.id_amp} - ID PRODUIT : {produit_amp.id_produit} - QUANTITE : {produit_amp.quantite} - UNITE : {produit_amp.unite} - USERNAME : {requete1.username}"
         content += "<br/>"
     content += "<br>"
     content += "<p>PARTICIPANTS</p>"
     content += "<br/>"
     for participant in participants_amp.Participants_amp.query.all():
         requete1 = users.User.query.filter(users.User.id==participant.id_user).first()
-        content += f"{participant.id_amp} - {requete1.username}"
+        content += f"ID AMPLET : {participant.id_amp} - USERNAME : {requete1.username}"
         content += "<br/>"
     return content
 
