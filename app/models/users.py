36a2d4aeca9b7e4a40a4ceed407a4cea886b9c82
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2022-01-05 08:52:04 +0100

    [FIX] Avatar

--- app/models/users.py
+++ app/models/users.py
@@ -2,6 +2,7 @@ from app import db
 from utils.sha import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from snowflake import SnowflakeGenerator
+import os
 
 gen = SnowflakeGenerator(0)
 
@@ -22,7 +23,14 @@ class User(db.Model, UserMixin):
         return self.role == 1
 
     def avatar_url(self):
-        return f"/r/a/{self.id}"
+        folder = f"static/avatar/{self.id}"
+        if not os.path.exists(folder):
+            return f"/r/a/{self.id}/0"
+        L = os.listdir(folder)
+        if len(L) == 0: 
+            return f"/r/a/{self.id}/0"
+        name = L[0].replace(".png", "")
+        return f"/r/a/{self.id}/{name}"
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
--- app/routes/avatar.py
+++ app/routes/avatar.py
@@ -1,34 +1,41 @@
 from db import app
 from flask_login import login_required, current_user
-from flask import render_template, request, send_from_directory
+from flask import render_template, request, send_from_directory, flash
 import os
+from snowflake import SnowflakeGenerator
 
 default_path = "photos/chien.jpeg"
+gen = SnowflakeGenerator(666)
 
 ##############################
 ########### AVATAR ###########
 ##############################
 
-@app.route('/change_avatar', methods = ['GET', 'POST'])
+@app.route('/pa', methods = ['GET', 'POST'])
 @login_required
 def upload_pp():
     if request.method == 'POST':
         f = request.files['file']
-        folder = f"static/avatar/{current_user.id}"
-        if not os.path.exists(folder):
-            os.mkdir(folder)
-        f.save(f"{folder}/avatar.png")
-        return 'avatar reçu !'
-    else:
-        return render_template('upload.html', personne=current_user)
+        if f:
+            folder = f"static/avatar/{current_user.id}"
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+            else:
+                L = os.listdir(folder)
+                for file in L:
+                    os.remove(f"{folder}/{file}")
+            f.save(f"{folder}/{next(gen)}.png")
+        else:
+            flash("Veuillez envoyer un fichier.")
+    return render_template('upload.html', user=current_user)
 
 if __name__=="__main__":
     app.run(debug=True)
 
-@app.route('/r/a/<string:id>')
-def get_pp(id):
-    folder = f"static/avatar/{id}"
-    file = f"avatar/{id}/avatar.png"
+@app.route('/r/a/<string:id>/<string:aid>')
+def get_pp(id,aid):
+    folder = f"static/avatar/{id}/{aid}.png"
+    file = f"avatar/{id}/{aid}.png"
     if not os.path.exists(folder):
         return send_from_directory('static', default_path)
     return send_from_directory('static', file)
