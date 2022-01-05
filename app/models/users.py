8832ee99d4ee3558171c01a8aa3ef01888d17a1f
Malo DAMIEN <elfamoso07@protonmail.com>
2022-01-05 01:20:44 +0000

    [MODIF] Profil
    Maintenant on peut modifier son adresse et son email. L'affichage de l'adresse sur son profil est maintenant bien faite. Sur le profil des autres on voit seulement une partie de l'adresse

--- app/models/users.py
+++ app/models/users.py
@@ -24,9 +24,6 @@ class User(db.Model, UserMixin):
     def avatar_url(self):
         return f"/r/a/{self.id}"
     
-    def avatar_url(self):
-        return f"/r/a/{self.id}"
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
