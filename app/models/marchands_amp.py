a90497613c8221c5f041b03823cdc7224f476de4
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2022-01-05 18:35:24 +0100

    [MODIF] Marchand amplet

--- app/models/marchands_amp.py
+++ app/models/marchands_amp.py
@@ -4,3 +4,4 @@ class Marchands_amp(db.Model):
     __tablename__ = 'marchands_amp'
     id_marchand = db.Column(db.String(40), db.ForeignKey('marchands.id'), primary_key=True)
     id_amp = db.Column(db.String(40), db.ForeignKey('amplets.id'), primary_key=True)
+    votes = db.Column(db.Float, nullable=True)
