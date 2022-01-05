6967bf3852d252b8cd742bf108b4321883885c70
Malo DAMIEN <elfamoso07@protonmail.com>
2022-01-05 18:41:42 +0000

    [FIX] Fonction votes

---
+++ app/routes/trajet.py
@@ -0,0 +1,18 @@
+from db import *
+from models import *
+from flask_login import login_required
+from flask_login import current_user
+from flask import render_template, request, flash
+from utils import vote_marchand
+
+
+@app.route('/trajet', methods=['GET','POST'])
+@login_required
+def trajet():
+    navettedisp = amplet.Amplets.query.filter_by(navette=True,ferme=True).order_by(amplet.Amplets.id).all()
+    phrase = "Il n'y pas de navette à optimiser"
+    for p in navettedisp:
+        a = vote_marchand.vote(p.id)
+        print(f"{p.id} et {a}")
+        phrase = f"{p.id} et {a}"
+    return render_template("trajet.html", user=current_user.id,phrase=phrase)
