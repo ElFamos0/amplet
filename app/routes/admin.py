051eb1cbb5c90c3471de96dd9e66e748871f211c
Malo DAMIEN <elfamoso07@protonmail.com>
2022-01-05 00:06:19 +0000

    [MODIF] Encore plus d'affichage dans le dashboard

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -26,7 +26,8 @@ def hello_world():
     content += "<br/>"
     for produit_amp in produits_amp.Produits_amp.query.all():
         requete1 = users.User.query.filter(users.User.id==produit_amp.id_user).first()
-        content += f"ID AMPLET : {produit_amp.id_amp} - ID PRODUIT : {produit_amp.id_produit} - QUANTITE : {produit_amp.quantite} - UNITE : {produit_amp.unite} - USERNAME : {requete1.username}"
+        requete2 = produits.Produits.query.filter(produits.Produits.id == produit_amp.id_produit).first()
+        content += f"ID AMPLET : {produit_amp.id_amp} - PRODUIT : {requete2.nom} - QUANTITE : {produit_amp.quantite} - UNITE : {produit_amp.unite} - USERNAME : {requete1.username}"
         content += "<br/>"
     content += "<br>"
     content += "<p>PARTICIPANTS</p>"
@@ -35,7 +36,13 @@ def hello_world():
         requete1 = users.User.query.filter(users.User.id==participant.id_user).first()
         content += f"ID AMPLET : {participant.id_amp} - USERNAME : {requete1.username}"
         content += "<br/>"
-    #vote_marchand.vote(6884187769132814339)
+    content += "<br>"
+    content += "<p>MARCHANDS</p>"
+    content += "<br/>"
+    for marchand in marchands.Marchands.query.all():
+        content += f"ID MARCHAND : {marchand.id} - NOM : {marchand.nom} - TYPE MARCHAND : {marchand.type}"
+        content += "<br/>"
+    #print(vote_marchand.vote("6884214503899140099"))
     return content
 
 @app.route("/admin", methods=['GET','POST'])
