80400f531a23cfb96f8eaf0c15a2c8d657e25d3d
Capelli LouisVincent <louis-vincent.capelli@telecomnancy.eu>
2022-01-05 02:28:58 +0100

    [MODIF] Mise en forme

--- app/routes/commande.py
+++ app/routes/commande.py
@@ -18,7 +18,11 @@ def commande():
     inscr_id_amp = []
     inscr_valide = []
     inscr_nom_coursier = []
-    inscriptions = participants_amp.Participants_amp.query.filter_by(id_user=current_user.id).join(amplet.Amplets,participants_amp.Participants_amp.id_amp==amplet.Amplets.id).filter_by(navette=0).all()
+    inscriptions = participants_amp.Participants_amp\
+        .query.filter_by(id_user=current_user.id)\
+        .join(amplet.Amplets,participants_amp.Participants_amp\
+        .id_amp==amplet.Amplets.id)\
+        .filter_by(navette=0).all()
     # print(inscriptions)
     if inscriptions:
         for i in range(len(inscriptions)):
@@ -40,7 +44,8 @@ def commande():
     cours_nom_participants = []
     cours_places_amp_tot = []
     cours_valide_participants_acceptes = []
-    coursier = amplet.Amplets.query.filter_by(id_coursier = current_user.id).all()
+    coursier = amplet.Amplets\
+        .query.filter_by(id_coursier = current_user.id).all()
     if coursier:
         for i in range(len(coursier)):
             cours_id_amp.append(coursier[i].id)
@@ -66,25 +71,26 @@ def commande():
     nav_list_len = []
     p = len(nav_id)
     for f in nav_id:
-        prod_choisis = produits_amp.Produits_amp.query.add_entity(produits.Produits).join(produits.Produits, produits_amp.Produits_amp.id_produit==produits.Produits.id).filter(produits_amp.Produits_amp.id_user==current_user.id,produits_amp.Produits_amp.id_amp==f).all()
-        print('\n\n\n')
-        print(prod_choisis)
-        print('\n\n\n')
+        prod_choisis = produits_amp.Produits_amp\
+            .query.add_entity(produits.Produits)\
+            .join(produits.Produits, produits_amp.Produits_amp.id_produit==produits.Produits.id)\
+            .filter(produits_amp.Produits_amp.id_user==current_user.id,produits_amp.Produits_amp.id_amp==f).all()
+        # print('\n\n\n')
+        # print(prod_choisis)
+        # print('\n\n\n')
         nav_id_produits_choisis.append([e[0].id_produit for e in prod_choisis if e[1].id_marchand in nav_id_marchands_choisis])
         nav_prix_produits_choisis.append([e[1].prix for e in prod_choisis if e[1].id_marchand in nav_id_marchands_choisis])
         nav_nom_produits_choisis.append([e[1].nom for e in prod_choisis if e[1].id_marchand in nav_id_marchands_choisis])
         nav_quantite_produits_choisis.append([e[0].quantite for e in prod_choisis if e[1].id_marchand in nav_id_marchands_choisis])
-        print('\n\n\n')
-        print(nav_quantite_produits_choisis)
-        print('\n\n\n')
+        # print('\n\n\n')
+        # print(nav_quantite_produits_choisis)
+        # print('\n\n\n')
         nav_unite_produits_choisis.append([e[0].unite for e in prod_choisis if e[1].id_marchand in nav_id_marchands_choisis])
         nav_date.append(timestamp.timestamp_to_date(amplet.Amplets.query.filter_by(id=f).first().date_depart,format='True'))
     nav_list_len = [len(e) for e in nav_id_produits_choisis]
-    
     # print('\n\n\n')
     # print(nav_id)
     # print('\n\n\n')
-    
     return render_template("commande.html",user=current_user,n=n,m=m,p=p,inscr_id_amp=inscr_id_amp, inscr_valide=inscr_valide,inscr_id_coursier=inscr_id_coursier,inscr_nom_coursier=inscr_nom_coursier, 
     cours_id_amp=cours_id_amp,cours_places_amp_occ=cours_places_amp_occ, cours_places_amp_tot=cours_places_amp_tot,cours_id_participants=cours_id_participants, cours_nom_participants=cours_nom_participants,cours_valide_participants=cours_valide_participants,cours_list_len=cours_list_len,
     nav_id=nav_id,nav_list_len=nav_list_len,nav_id_produits_choisis=nav_id_produits_choisis,nav_prix_produits_choisis=nav_prix_produits_choisis,nav_nom_produits_choisis=nav_nom_produits_choisis,nav_quantite_produits_choisis=nav_quantite_produits_choisis,nav_unite_produits_choisis=nav_unite_produits_choisis,nav_date=nav_date)
